e3d42119fada39e41896d9ff36333b538e20af8f
IvyGao <ivvvygao@163.com>
2020-06-23 16:40:41 +0800

    static-utils

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,12 +9,16 @@
 import HomeHeader from "./layout/Header";
 import HomeSubject from "./layout/Subject";
 import HomeContent from "./layout/Content";
+import {screenSize} from "../assets/js/utils.js";
 export default {
   name: "Home",
   components: {
     HomeHeader,
     HomeSubject,
     HomeContent
+  },
+  mounted(){
+  	screenSize(this.$refs.editor);
   }
 }
 </script>
