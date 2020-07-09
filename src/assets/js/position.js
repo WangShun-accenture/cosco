8b7d32f7bc25f6f8dd0501cc9e08e0d58ff923c0
于鸿飞 <>
2020-07-09 16:50:53 +0800

    add images

---
+++ src/assets/js/position.js
@@ -0,0 +1,40 @@
+var geoCoordMap = {
+    "Amsterdam": [4.895168, 52.370216],
+    "Athens": [-83.357567, 33.951935],
+    "Auckland": [174.763332, -36.84846],
+    "Bangkok": [100.501765, 13.756331],
+    "Barcelona": [2.173403, 41.385064],
+    "Beijing": [116.407395, 39.904211],
+    "Berlin": [13.404954, 52.520007],
+    "Bogotá": [-74.072092, 4.710989],
+    "Bratislava": [17.107748, 48.148596],
+    "Brussels": [4.35171, 50.85034],
+    "Budapest": [19.040235, 47.497912],
+    "Buenos Aires": [-58.381559, -34.603684],
+    "Bucharest": [26.102538, 44.426767],
+    "Caracas": [-66.903606, 10.480594],
+    "Chicago": [-87.629798, 41.878114],
+};
+
+var rawData = [
+    ["Amsterdam"],
+    ["Athens"],
+    ["Auckland"],
+    ["Bangkok"],
+    ["Barcelona"],
+    ["Beijing"],
+    ["Berlin"],
+    ["Bogotá"],
+    ["Bratislava"],
+    ["Brussels"],
+    ["Budapest"],
+    ["Buenos Aires"],
+    ["Bucharest"],
+    ["Caracas"],
+    ["Chicago"]
+];
+
+export {
+    geoCoordMap,
+    rawData
+};
